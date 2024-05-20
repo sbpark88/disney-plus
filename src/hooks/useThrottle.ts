d7750ad3f18e0e6692471499535b91abc6efe512
sbpark88 <saebyulpark88@gmail.com>
2024-05-20 22:16:27 +0900

    Add: Append some performance hooks

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef, useState } from "react";
+
+const useThrottle = <T>(value: T, delay = 500): T => {
+  const [throttledValue, setThrottledValue] = useState(value);
+  const available = useRef(true);
+
+  useEffect(() => {
+    if (available.current) {
+      console.log(value, available.current);
+      available.current = false;
+      setThrottledValue(value);
+      setTimeout(() => (available.current = true), delay);
+    }
+  }, [value, delay]);
+
+  return throttledValue;
+};
+
+export default useThrottle;
